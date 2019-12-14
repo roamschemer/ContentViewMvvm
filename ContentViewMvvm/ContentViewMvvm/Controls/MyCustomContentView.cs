949af804c8b2d6fb3d6fe7eba054dcf035f9a823
放浪 軍師 <roamschemer@gmail.com>
2019-12-14 13:43:32 +0900

    コメント追加。無駄削除。

--- ContentViewMvvm/ContentViewMvvm/Controls/MyCustomContentView.cs
+++ ContentViewMvvm/ContentViewMvvm/Controls/MyCustomContentView.cs
@@ -1,22 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace ContentViewMvvm.Controls
 {
     public class MyCustomContentView : ContentView
     {
         public static readonly BindableProperty NowNameProperty =
-            BindableProperty.Create("NowName",
-                                    typeof(string),
-                                    typeof(MyCustomContentView),
-                                    string.Empty,
-                                    propertyChanged: (bindable, oldValue, newValue) =>
+            BindableProperty.Create(nameof(NowName), //名前
+                                    typeof(string),　//型
+                                    typeof(MyCustomContentView), //自クラス
+                                    string.Empty,    //初期値
+                                    propertyChanged: (bindable, oldValue, newValue) => //変更があったことを感知するイベントハンドラ
                                     {
                                         ((MyCustomContentView)bindable).NowName = newValue;
                                     },
-                                    defaultBindingMode: BindingMode.TwoWay);
+                                    defaultBindingMode: BindingMode.TwoWay); //初期バインディング方向
         public object NowName
         {
             get => GetValue(NowNameProperty);
